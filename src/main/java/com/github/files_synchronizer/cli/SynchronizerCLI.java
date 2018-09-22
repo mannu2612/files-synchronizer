7d277ed84bc997f51e4e43bf4da4c2a62bba0f11
Manoj <mannu2612@gmail.com>
2018-09-22 16:58:20 +0530

    Completion message added

--- src/main/java/com/github/files_synchronizer/cli/SynchronizerCLI.java
+++ src/main/java/com/github/files_synchronizer/cli/SynchronizerCLI.java
@@ -20,11 +20,16 @@ public class SynchronizerCLI {
 			if (line.hasOption(CLIOptions.COPY)) {
 				String src = line.getOptionValues(CLIOptions.COPY)[0];
 				String dest = line.getOptionValues(CLIOptions.COPY)[1];
+				if(synchronizer.syncAndCopyFromSourceToDest(src, dest)) {
+					System.out.println("Synchronized and Copied from "+src+" to "+dest);
+				}
 				synchronizer.syncAndCopyFromSourceToDest(src, dest);
 			} else if (line.hasOption(CLIOptions.MOVE)) {
 				String src = line.getOptionValues(CLIOptions.MOVE)[0];
-				String dest = line.getOptionValues(CLIOptions.MOVE)[1];
-				synchronizer.syncAndMoveFromSourceToDest(src, dest);
+				String dest = line.getOptionValues(CLIOptions.MOVE)[1];				
+				if(synchronizer.syncAndMoveFromSourceToDest(src, dest)) {
+					System.out.println("Synchronized and Moved from "+src+" to "+dest);
+				}
 			}
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
